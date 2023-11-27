52c5e95d2e13ec1a934f890fca7b98914c174958
Heitor <49591729+hectorclarkag@users.noreply.github.com>
2023-11-27 18:03:41 +0000

    footer 1:51:45

--- spotify-clone/src/components/Footer.jsx
+++ spotify-clone/src/components/Footer.jsx
@@ -13,4 +13,9 @@ const Container = styled.div`
     height: 100%;
     width: 100%;
     border-top: 1px solid #282828; 
+    display: grid;
+    grid-template-columns: 1fr 2fr 1fr;
+    align-items: center;
+    justify-content: center;
+    padding: 0 1rem;
 `;
